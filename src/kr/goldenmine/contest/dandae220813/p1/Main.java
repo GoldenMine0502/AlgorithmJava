a3b9caf6db14b4d8af17527a4f2ed05aa8437723
GoldenMine0502 <ehe123@naver.com>
2022-08-29 20:25:38 +0900

    대회문제풀이

---
+++ src/kr/goldenmine/contest/dandae220813/p1/Main.java
@@ -0,0 +1,79 @@
+package kr.goldenmine.contest.dandae220813.p1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        long N = scan.nextInt();
+        long M = scan.nextInt();
+        long a = scan.nextInt();
+        long K = scan.nextInt();
+
+        // 30팀이 각 5명, 51명이 남았고 우리는 3명
+
+        // (51 - 3) / 5 = 9.x
+        // 최악: 모든 팀이 죽고, 남은 팀이 싹다 5명씩 남은 경우
+        //
+    }
+}
