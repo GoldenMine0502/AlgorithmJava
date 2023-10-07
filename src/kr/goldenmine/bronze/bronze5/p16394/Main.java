0f91f7b29600034593e933977b7c04b5332adaf3
GoldenMine0502 <ehe123@naver.com>
2023-10-07 19:06:23 +0900

    16394번 브론즈5

---
+++ src/kr/goldenmine/bronze/bronze5/p16394/Main.java
@@ -0,0 +1,12 @@
+package kr.goldenmine.bronze.bronze5.p16394;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+
+        System.out.println(year - 1946);
+    }
+}
