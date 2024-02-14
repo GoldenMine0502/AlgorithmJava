b5b88eb70080bfd49a1741801898cdaf4aa06061
GoldenMine0502 <ehe123@naver.com>
2024-02-14 19:43:18 +0900

    1715번 골드4 그리디

---
+++ src/kr/goldenmine/baekjoon/gold/gold4/p1715S/Main.java
@@ -0,0 +1,84 @@
+package kr.goldenmine.baekjoon.gold.gold4.p1715S;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        int N = scan.nextInt();
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            priorityQueue.add(scan.nextInt());
+        }
+
+        int sum = 0;
+        while(priorityQueue.size() >= 2) {
+            int add = priorityQueue.poll() + priorityQueue.poll();
+            sum += add;
+            priorityQueue.add(add);
+        }
+        System.out.println(sum);
+    }
+}
