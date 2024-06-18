000a556c8f176a07728da869b4fe48190fbf264e
GoldenMine0502 <ehe123@naver.com>
2024-06-18 18:21:05 +0900

    20044번 그리디

---
+++ src/kr/goldenmine/baekjoon/silver/silver4/p20044/Main.java
@@ -0,0 +1,86 @@
+package kr.goldenmine.baekjoon.silver.silver4.p20044;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        int N = scan.nextInt() * 2;
+        int[] li = new int[N];
+        for(int i = 0; i < N; i++) {
+            li[i] = scan.nextInt();
+        }
+
+        Arrays.sort(li);
+//        System.out.println(Arrays.toString(li));
+
+        int min = 99999999;
+
+        for(int i = 0; i < N / 2; i++) {
+            min = Math.min(min, li[i] + li[N - i - 1]);
+//            System.out.println(min);
+        }
+        System.out.println(min);
+    }
+}
