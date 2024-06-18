c47ef1d4d4c35e364eca2db36343a43bbe652d75
GoldenMine0502 <ehe123@naver.com>
2024-06-18 18:20:07 +0900

    2012번 그리디

---
+++ src/kr/goldenmine/baekjoon/silver/silver3/p2012S/Main.java
@@ -0,0 +1,85 @@
+package kr.goldenmine.baekjoon.silver.silver3.p2012S;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        int N = scan.nextInt();
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        Arrays.sort(arr);
+//        System.out.println(Arrays.toString(arr));
+
+        long fuck = 0;
+        for(int i = 1; i <= N; i++) {
+            fuck += Math.abs(arr[i - 1] - i);
+        }
+        System.out.println(fuck);
+    }
+}
