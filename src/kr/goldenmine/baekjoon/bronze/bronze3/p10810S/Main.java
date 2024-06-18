9a04362e568a47b8170f0193dc6c3206089bfff9
GoldenMine0502 <ehe123@naver.com>
2024-06-18 18:11:23 +0900

    10810번 구현

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze3/p10810S/Main.java
@@ -0,0 +1,87 @@
+package kr.goldenmine.baekjoon.bronze.bronze3.p10810S;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+        int[] arr = new int[N + 1];
+
+        for(int i = 0; i < M; i++) {
+            int I = scan.nextInt();
+            int J = scan.nextInt();
+            int K = scan.nextInt();
+
+            for(int j = I; j <= J; j++) {
+                arr[j] = K;
+            }
+        }
+
+        for(int i = 1; i <= N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
