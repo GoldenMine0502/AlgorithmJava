89030e2e0e4aa10c5f8df26ad5aa834b9d8df789
GoldenMine0502 <ehe123@naver.com>
2024-06-18 18:17:03 +0900

    12728번 수학 (실패)

--- src/kr/goldenmine/baekjoon/platinum/platinum1/p12728F/Main.java
+++ src/kr/goldenmine/baekjoon/platinum/platinum1/p12728F/Main.java
@@ -2,15 +2,49 @@ package kr.goldenmine.baekjoon.platinum.platinum1.p12728F;
 
 import java.math.BigDecimal;
 //import java.math.MathContext;
+import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
+    private static final BigDecimal SQRT_DIG = new BigDecimal(150);
+    private static final BigDecimal SQRT_PRE = new BigDecimal(10).pow(SQRT_DIG.intValue());
+
+    /**
+     * Private utility method used to compute the square root of a BigDecimal.
+     *
+     * @author Luciano Culacciatti
+     * @url http://www.codeproject.com/Tips/257031/Implementing-SqrtRoot-in-BigDecimal
+     */
+    private static BigDecimal sqrtNewtonRaphson  (BigDecimal c, BigDecimal xn, BigDecimal precision){
+        BigDecimal fx = xn.pow(2).add(c.negate());
+        BigDecimal fpx = xn.multiply(new BigDecimal(2));
+        BigDecimal xn1 = fx.divide(fpx,2*SQRT_DIG.intValue(),RoundingMode.HALF_DOWN);
+        xn1 = xn.add(xn1.negate());
+        BigDecimal currentSquare = xn1.pow(2);
+        BigDecimal currentPrecision = currentSquare.subtract(c);
+        currentPrecision = currentPrecision.abs();
+        if (currentPrecision.compareTo(precision) <= -1){
+            return xn1;
+        }
+        return sqrtNewtonRaphson(c, xn1, precision);
+    }
+
+    /**
+     * Uses Newton Raphson to compute the square root of a BigDecimal.
+     *
+     * @author Luciano Culacciatti
+     * @url http://www.codeproject.com/Tips/257031/Implementing-SqrtRoot-in-BigDecimal
+     */
+    public static BigDecimal bigSqrt(BigDecimal c){
+        return sqrtNewtonRaphson(c,new BigDecimal(1),new BigDecimal(1).divide(SQRT_PRE));
+    }
+
     public static void main(String[] args) {
 //        final int MAX = 100;
         final BigDecimal THREE = new BigDecimal("3");
         final BigDecimal FIVE = new BigDecimal("5");
-        final BigDecimal FIVE_SQRT = new BigDecimal(String.valueOf(Math.sqrt(5)));
+        final BigDecimal FIVE_SQRT = bigSqrt(FIVE);
 //        final BigDecimal base = THREE.add(FIVE.sqrt(new MathContext(100, RoundingMode.FLOOR)));
         final BigDecimal base = THREE.add(FIVE_SQRT);
 
@@ -23,7 +57,8 @@ public class Main {
         for(int i = 0; i < T; i++) {
             int N = scan.nextInt();
 
-            String result = pow(base, N).toBigInteger().toString();
+            String result = pow(base, N).remainder(mod, MathContext.UNLIMITED).toBigInteger().toString();
+            System.out.println(result);
             if(result.length() == 1) {
                 System.out.println("Case #" + (i + 1) + ": 00" + result);
             } else if(result.length() == 2) {
@@ -43,16 +78,26 @@ public class Main {
         }
 
         if(exponent == 2) {
-            return base.multiply(base);
+            return base.multiply(base, MathContext.UNLIMITED);
         }
 
-        BigDecimal half = pow(base, exponent / 2).remainder(mod).setScale(100, RoundingMode.FLOOR);
-//        System.out.println(half);
+        BigDecimal half = pow(base, exponent / 2).setScale(50, RoundingMode.HALF_UP);
 
-        if(exponent % 2 == 0) {
-            return (half.multiply(half)).remainder(mod);
-        } else {
-            return (half.multiply(half).multiply(base)).remainder(mod);
+//        if(half.compareTo(mod) > 0) {
+//            half = half.remainder(mod, MathContext.UNLIMITED);
+//            System.out.println("d" + exponent + ", " + half);
+//        }
+
+        BigDecimal full = half.multiply(half, MathContext.UNLIMITED);
+        if(exponent % 2 == 1) {
+            full = full.multiply(base, MathContext.UNLIMITED);
         }
+        System.out.println(exponent + ", " + full);
+//        if(full.compareTo(mod) > 0) {
+//            full = full.remainder(mod, MathContext.UNLIMITED);
+//            System.out.println("d" + exponent + ", " + full);
+//        }
+
+        return full;
     }
 }
