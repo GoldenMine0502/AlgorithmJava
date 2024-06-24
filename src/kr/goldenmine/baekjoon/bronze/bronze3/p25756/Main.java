0cf9ba0673f4c90fb1b1a2a989e0aee61fb20db5
GoldenMine0502 <ehe123@naver.com>
2024-06-24 12:20:01 +0900

    25756번 구현

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze3/p25756/Main.java
@@ -0,0 +1,81 @@
+package kr.goldenmine.baekjoon.bronze.bronze3.p25756;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        double immune = 0D;
+
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int n = scan.nextInt();
+
+            double nextImmune = 1 - (1 - immune) * (1 - n / 100.0);
+
+            System.out.println(nextImmune * 100);
+            immune = nextImmune;
+        }
+    }
+}
