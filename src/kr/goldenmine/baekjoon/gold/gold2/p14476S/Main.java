0ea992cdc8c01a6a1de8849a47d0907cddf1d5e4
GoldenMine0502 <ehe123@naver.com>
2024-06-28 21:53:46 +0900

    14476번 누적합 (시간 1등)

--- src/kr/goldenmine/baekjoon/gold/gold2/p14476S/Main.java
+++ src/kr/goldenmine/baekjoon/gold/gold2/p14476S/Main.java
@@ -2,66 +2,23 @@ package kr.goldenmine.baekjoon.gold.gold2.p14476S;
 
 public class Main {
     static class Reader {
-        final int SIZE = 1 << 13;
+        final int SIZE = 1 << 15;
         byte[] buffer = new byte[SIZE];
         int index, size;
-
-        String nextString() throws Exception {
-            StringBuilder sb = new StringBuilder();
-            byte c;
-            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
-            do sb.appendCodePoint(c);
-            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
-            return sb.toString();
-        }
-
-        char nextChar() throws Exception {
-            byte c;
-            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
-            return (char)c;
-        }
 
         int nextInt() throws Exception {
             int n = 0;
             byte c;
-            boolean isMinus = false;
-            while ((c = read()) <= 32) { if (size < 0) return -1; }
-            if (c == 45) { c = read(); isMinus = true; }
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
             do n = (n << 3) + (n << 1) + (c & 15);
             while (isNumber(c = read()));
-            return isMinus ? ~n + 1 : n;
-        }
-
-        long nextLong() throws Exception {
-            long n = 0;
-            byte c;
-            boolean isMinus = false;
-            while ((c = read()) <= 32);
-            if (c == 45) { c = read(); isMinus = true; }
-            do n = (n << 3) + (n << 1) + (c & 15);
-            while (isNumber(c = read()));
-            return isMinus ? ~n + 1 : n;
-        }
-
-        double nextDouble() throws Exception {
-            double n = 0, div = 1;
-            byte c;
-            boolean isMinus = false;
-            while ((c = read()) <= 32) { if (size < 0) return -12345; }
-            if (c == 45) { c = read(); isMinus = true; }
-            else if (c == 46) { c = read(); }
-            do n = (n * 10) + (c & 15);
-            while (isNumber(c = read()));
-            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
-            return isMinus ? -n : n;
+            return n;
         }
 
         boolean isNumber(byte c) {
             return 47 < c && c < 58;
-        }
-
-        boolean isAlphabet(byte c){
-            return (64 < c && c < 91) || (96 < c && c < 123);
         }
 
         byte read() throws Exception {
@@ -74,7 +31,7 @@ public class Main {
     }
 
     public static int gcd(int a, int b) {
-        while(b != 0) {
+        while (b != 0) {
             int gcd = a % b;
             a = b;
             b = gcd;
@@ -88,52 +45,39 @@ public class Main {
 
         int N = scan.nextInt();
 
-        int[] arr = new int[N + 1];
-        int[] gcds = new int[N + 1];
-        int[] gcds_reverse = new int[N + 1];
+        int[] arr = new int[100_0001];
+        int[] gcds = new int[100_0002];
+        int[] gcds_reverse = new int[100_0002];
 
-        for(int i = 1; i <= N; i++) {
-            arr[i] = scan.nextInt();
-            if(i != 1) {
-                gcds[i] = gcd(gcds[i - 1], arr[i]);
-            } else {
-                gcds[i] = arr[1];
-            }
+        arr[0] = arr[1] = gcds[1] = scan.nextInt();
+        for (int i = 2; i <= N; i++) {
+            gcds[i] = gcd(gcds[i - 1], arr[i] = scan.nextInt());
         }
 
-        for(int i = N; i >= 1; i--) {
-            if(i == N) {
-                gcds_reverse[N] = arr[N];
-            } else {
-                gcds_reverse[i] = gcd(gcds_reverse[i + 1], arr[i]);
-            }
+        gcds_reverse[N + 1] = gcds[N - 1];
+
+        gcds_reverse[N] = arr[N];
+        for (int i = N - 1; i >= 1; i--) {
+            gcds_reverse[i] = gcd(gcds_reverse[i + 1], arr[i]);
         }
+
+        gcds[0] = gcds_reverse[2];
 
         int max = 0;
         int removedIndex = -1;
-        for(int i = 1; i <= N; i++) {
-            int nextValue;
-            if(i == 1) {
-                nextValue = gcds_reverse[2]; // 2~N번째까지의 최대공약수
-            } else if(i == N) {
-                nextValue = gcds[N - 1]; //1~N-1번째까지의 최대공약수
-            } else {
-                nextValue = gcd(gcds[i - 1], gcds_reverse[i + 1]); // N번째까지 제외하고 최대공약수
-            }
+        for (int i = 1; i <= N; i++) {
+            int nextValue = gcd(gcds[i - 1], gcds_reverse[i + 1]);
 
-            if(max < nextValue) {
+            if (max < nextValue) {
                 removedIndex = i;
                 max = nextValue;
             }
         }
 
-        int removedValue = arr[removedIndex];
-        if(removedValue % max == 0) { // 나눠 떨어지면 약수
+        if (arr[removedIndex] % max == 0) { // 나눠 떨어지면 약수
             System.out.println(-1);
         } else { // 나눠 떨어지지 않으면 약수 아님
-            System.out.println(max + " " + arr[removedIndex]);
+            System.out.println(String.valueOf(max) + ' ' + arr[removedIndex]);
         }
-//        System.out.println(Arrays.toString(gcds));
-//        System.out.println(Arrays.toString(gcds_reverse));
     }
 }
