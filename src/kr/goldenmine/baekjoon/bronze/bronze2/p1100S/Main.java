68de0d81d3e5e56d9e83a6f194be8e3a17949f4a
GoldenMine0502 <ehe123@naver.com>
2024-06-28 19:23:11 +0900

    1100번 구현 (숏코딩)

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze2/p1100S/Main.java
@@ -0,0 +1,3 @@
+package kr.goldenmine.baekjoon.bronze.bronze2.p1100S;
+
+interface Main{static void main(String[]s)throws Exception{byte[]b=new byte[73];System.in.read(b,0,72);int c=0,i=0;for(;i<72;i+=2)if(b[i]==70)c++;System.out.print(c);}}
