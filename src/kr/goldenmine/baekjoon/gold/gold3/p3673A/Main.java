260532324ee0ff5f04fcc587daa7757c11bdb0d0
GoldenMine0502 <ehe123@naver.com>
2024-07-02 00:02:31 +0900

    3673번 누적합

---
+++ src/kr/goldenmine/baekjoon/gold/gold3/p3673A/Main.java
@@ -0,0 +1,96 @@
+package kr.goldenmine.baekjoon.gold.gold3.p3673A;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char)c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32);
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -12345; }
+            if (c == 45) { c = read(); isMinus = true; }
+            else if (c == 46) { c = read(); }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        int T = scan.nextInt();
+
+        while(T-- > 0) {
+            int D = scan.nextInt();
+            int N = scan.nextInt();
+
+            long[] arr = new long[N + 1];
+            int[] mod = new int[D];
+            for(int i = 1; i <= N; i++) {
+                arr[i] = arr[i - 1] + scan.nextInt();
+                mod[(int)(arr[i] % D)]++;
+            }
+//            System.out.println(Arrays.toString(arr));
+//            System.out.println(Arrays.toString(mod));
+            long sum = mod[0];
+            for(int i = 0; i < D; i++) {
+                if(mod[i] >= 2) sum += ((long) mod[i] * (mod[i] - 1)) / 2;
+            }
+            System.out.println(sum);
+        }
+    }
+}
