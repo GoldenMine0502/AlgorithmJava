fd366587ee8a52f02688c4d758cdbad47e28efa3
GoldenMine0502 <ehe123@naver.com>
2024-07-02 03:30:19 +0900

    빠른 출력 템플릿 추가

--- src/kr/goldenmine/templates/FastOutputs.java
+++ src/kr/goldenmine/templates/FastOutputs.java
@@ -3,6 +3,7 @@ package kr.goldenmine.templates;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class FastOutputs {
     public static void main(String[] args) throws IOException {
@@ -16,5 +17,56 @@ public class FastOutputs {
         }
 
         output.flush();
+    }
+
+    // 기본적으로 BufferedWriter에는 모든 메소드에 synchronized가 붙어있으므로 최대한 synchronized를 빼주도록 작동
+    static class UnsynchronizedWriter extends BufferedWriter {
+        char[] buf;
+        int nChars;
+        int index;
+        Writer out;
+
+        char[] bufForWriteNumber = new char[22];
+
+        UnsynchronizedWriter(Writer out, int size) {
+            super(out);
+            this.out = out;
+            buf = new char[size];
+            nChars = size;
+        }
+
+        public void writeInt(int n) {
+
+        }
+
+        public void writeString(String s) throws IOException {
+            for(int i = 0; i < s.length(); i++) {
+                write(s.charAt(i));
+            }
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            for(int i = off; i < off + len; i++) {
+                write(cbuf[i]);
+            }
+        }
+
+        public void write(int c) throws IOException {
+            if (index >= nChars) flush();
+            buf[index++] = (char) c;
+        }
+
+        @Override
+        public void flush() throws IOException {
+            out.write(buf, 0, index);
+            index = 0;
+            out.flush(); // TODO 여기에 synchronized가 달려있어서 없애야한다
+        }
+
+        @Override
+        public void close() throws IOException {
+            flush();
+        }
     }
 }
