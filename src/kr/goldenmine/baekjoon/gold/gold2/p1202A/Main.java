bc78f52c2978216ddbf034dbc18bbf9b7fcfe215
GoldenMine0502 <ehe123@naver.com>
2024-07-02 00:02:23 +0900

    1202번 그리디, 우선순위 큐

--- src/kr/goldenmine/baekjoon/gold/gold2/p1202A/Main.java
+++ src/kr/goldenmine/baekjoon/gold/gold2/p1202A/Main.java
@@ -1,10 +1,11 @@
-package kr.goldenmine.baekjoon.gold.gold2.p1202;
+package kr.goldenmine.baekjoon.gold.gold2.p1202A;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -90,9 +91,7 @@ public class Main {
 
         Jewel[] jewels = new Jewel[N];
         for(int i = 0; i < N; i++) {
-            int weight = scan.nextInt();
-            int value = scan.nextInt();
-            jewels[i] = new Jewel(weight, value);
+            jewels[i] = new Jewel(scan.nextInt(), scan.nextInt());
         }
 
         int[] bags = new int[K];
@@ -100,23 +99,30 @@ public class Main {
             bags[i] = scan.nextInt();
         }
 
-        Arrays.sort(jewels, new Comparator<Jewel>() {
-            @Override
-            public int compare(Jewel o1, Jewel o2) {
-                if(o1.value != o2.value) {
-                    return -Integer.compare(o1.value, o2.value);
-                } else {
-                    return Integer.compare(o1.weight, o2.weight);
-                }
+        Arrays.sort(jewels, (o1, o2) -> {
+            int c1 = Integer.compare(o1.weight, o2.weight);
+            if(c1 == 0) {
+                return Integer.compare(o1.value, o2.value);
             }
+
+            return c1;
         });
 
-        int[] results = new int[N];
-        int index = 0;
-
         Arrays.sort(bags);
+        PriorityQueue<Jewel> queue = new PriorityQueue<>(Comparator.comparingInt(o -> -o.value));
 
-        System.out.println(Arrays.toString(jewels));
-        System.out.println(Arrays.toString(bags));
+        int index = 0;
+        long sum = 0;
+        for(int i = 0; i < K; i++) {
+            while(index < N && bags[i] >= jewels[index].weight) {
+                queue.add(jewels[index]);
+                index++;
+            }
+            if(!queue.isEmpty()) {
+                sum += queue.poll().value;
+            }
+        }
+
+        System.out.println(sum);
     }
 }
