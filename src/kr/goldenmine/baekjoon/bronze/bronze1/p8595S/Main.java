ca753880ac6000d17c0175278bb99024f651caf1
GoldenMine0502 <ehe123@naver.com>
2024-07-04 02:02:48 +0900

    8595번 구현

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze1/p8595S/Main.java
@@ -0,0 +1,64 @@
+package kr.goldenmine.baekjoon.bronze.bronze1.p8595S;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        scan.nextInt();
+
+        String str = scan.nextString();
+        long total = 0;
+        int current = 0;
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if('0' <= ch && ch <= '9') {
+                current *= 10;
+                current += (ch - '0');
+            } else {
+                total += current;
+                current = 0;
+            }
+        }
+        total += current;
+        System.out.println(total);
+    }
+}
