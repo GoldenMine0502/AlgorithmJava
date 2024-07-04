74eb6c1ae28e9fdd2c432bee71434c3b138e65cf
GoldenMine0502 <ehe123@naver.com>
2024-07-04 20:18:54 +0900

    22233번 해시

---
+++ src/kr/goldenmine/baekjoon/silver/silver3/p22233/Main.java
@@ -0,0 +1,203 @@
+package kr.goldenmine.baekjoon.silver.silver3.p22233;
+
+import java.io.*;
+import java.util.HashSet;
+
+public class Main {
+    final private static int BUFFER_SIZE = 1 << 19;
+    private static DataInputStream din = new DataInputStream(System.in);
+    private static byte[] buffer = new byte[BUFFER_SIZE];
+    private static int bufferPointer = 0, bytesRead = 0;
+    private static byte[] buf = new byte[111]; // line length
+
+    public static String readLine() throws IOException {
+        int cnt = 0, c;
+        while ((c = read()) != -1) {
+            if (c == '\n') {
+                if (cnt != 0) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            buf[cnt++] = (byte) c;
+        }
+        return new String(buf, 0, cnt);
+    }
+
+    public static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    public static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    public static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ')
+            c = read();
+        boolean neg = (c == '-');
+        if (neg)
+            c = read();
+
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (c == '.') {
+            while ((c = read()) >= '0' && c <= '9') {
+                ret += (c - '0') / (div *= 10);
+            }
+        }
+
+        if (neg)
+            return -ret;
+        return ret;
+    }
+
+    private static void fillBuffer() throws IOException {
+        bytesRead = din.read(buffer, bufferPointer = 0,
+                BUFFER_SIZE);
+        if (bytesRead == -1)
+            buffer[0] = -1;
+    }
+
+    private static byte read() throws IOException {
+        if (bufferPointer == bytesRead)
+            fillBuffer();
+        return buffer[bufferPointer++];
+    }
+
+    public static void close() throws IOException {
+        if (din == null)
+            return;
+        din.close();
+    }
+
+    // 기본적으로 BufferedWriter에는 모든 메소드에 synchronized가 붙어있으므로 최대한 synchronized를 빼주도록 작동
+    static class Writer extends BufferedWriter {
+        char[] buf;
+        int nChars;
+        int index;
+
+        char[] bufForWriteNumber = new char[22];
+
+        Writer() {
+            this(1 << 10);
+        }
+
+        Writer(int size) {
+            super(new OutputStreamWriter(System.out));
+            buf = new char[size];
+            nChars = size;
+        }
+
+        public void writeInt(int n) throws IOException {
+            boolean minus = n < 0;
+            if(minus) n = ~n + 1;
+
+            int d;
+            for(d = 10 - 1; d >= 0; d--) {
+                bufForWriteNumber[d] = (char) (n % 10 + '0');
+                n /= 10;
+                if(n == 0) break;
+            }
+
+            if(minus) {
+                bufForWriteNumber[--d] = '-';
+            }
+
+            for(; d < 10; d++) {
+                write(bufForWriteNumber[d]);
+            }
+        }
+
+        public void writeString(String s) throws IOException {
+            for(int i = 0; i < s.length(); i++) {
+                write(s.charAt(i));
+            }
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            for(int i = off; i < off + len; i++) {
+                write(cbuf[i]);
+            }
+        }
+
+        public void write(int c) throws IOException {
+            if (index >= nChars) flush();
+            buf[index++] = (char) c;
+        }
+
+        @Override
+        public void flush() throws IOException {
+            super.write(buf, 0, index);
+            index = 0;
+            super.flush(); // TODO 여기에 synchronized가 달려있어서 없애야한다
+        }
+
+        @Override
+        public void close() throws IOException {
+            flush();
+            super.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = nextInt();
+        int M = nextInt();
+
+        HashSet<String> set = new HashSet<>(N);
+
+        for (int i = 0; i < N; i++) {
+            set.add(readLine());
+        }
+
+        Writer output = new Writer();
+        int cnt = 0, c;
+        for (int i = 0; i < M; i++) {
+            while ((c = read()) != -1) {
+                if (c == ',' || c == '\n') {
+                    set.remove(new String(buf, 0, cnt));
+                    cnt = 0;
+
+                    if (c == '\n') break;
+                } else {
+                    buf[cnt++] = (byte) c;
+                }
+            }
+            output.writeInt(set.size());
+            output.write('\n');
+        }
+        output.close();
+    }
+}
