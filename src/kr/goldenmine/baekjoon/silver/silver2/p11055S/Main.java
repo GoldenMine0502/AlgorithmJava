e53b321b22e363e779973720d985a0180fa13ae6
GoldenMine0502 <ehe123@naver.com>
2024-07-08 00:46:50 +0900

    11055번 DP, 20162 같은 논리로 풀림

--- src/kr/goldenmine/baekjoon/silver/silver2/p11055S/Main.java
+++ src/kr/goldenmine/baekjoon/silver/silver2/p11055S/Main.java
@@ -88,13 +88,11 @@ public class Main {
         for(int i = 1; i <= N; i++) {
             dp[i] = arr[i];
             for(int j = 1; j < i; j++) {
-                if(arr[j] < arr[i]) { // 이 조건을 만족하면 증가 가능
+                if(arr[j] < arr[i]) // 이 조건을 만족하면 증가 가능
                     dp[i] = Math.max(dp[i], dp[j] + arr[i]);
-                }
             }
             max = Math.max(max, dp[i]);
         }
-//        System.out.println(Arrays.toString(dp));
         System.out.println(max);
     }
 }
