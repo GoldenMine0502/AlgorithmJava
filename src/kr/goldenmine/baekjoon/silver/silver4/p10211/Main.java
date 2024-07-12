05701d3f72d499883277aa6835dfb8fa2c86c86e
GoldenMine0502 <ehe123@naver.com>
2024-07-13 04:10:20 +0900

    10211 DP

---
+++ src/kr/goldenmine/baekjoon/silver/silver4/p10211/Main.java
@@ -0,0 +1,130 @@
+package kr.goldenmine.baekjoon.silver.silver4.p10211;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 11;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    // 기본적으로 BufferedWriter에는 모든 메소드에 synchronized가 붙어있으므로 최대한 synchronized를 빼주도록 작동
+    static class Writer extends BufferedWriter {
+        char[] buf;
+        int nChars;
+        int index;
+
+        char[] bufForWriteNumber = new char[22];
+
+        Writer() {
+            this(1 << 9);
+        }
+
+        Writer(int size) {
+            super(new OutputStreamWriter(System.out));
+            buf = new char[size];
+            nChars = size;
+        }
+
+        public void writeInt(int n) throws IOException {
+            boolean minus = n < 0;
+            if(minus) n = ~n + 1;
+
+            int d;
+            for(d = 10 - 1; d >= 0; d--) {
+                bufForWriteNumber[d] = (char) (n % 10 + '0');
+                n /= 10;
+                if(n == 0) break;
+            }
+
+            if(minus) {
+                bufForWriteNumber[--d] = '-';
+            }
+
+            for(; d < 10; d++) {
+                write(bufForWriteNumber[d]);
+            }
+        }
+
+        public void writeString(String s) throws IOException {
+            for(int i = 0; i < s.length(); i++) {
+                write(s.charAt(i));
+            }
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            for(int i = off; i < off + len; i++) {
+                write(cbuf[i]);
+            }
+        }
+
+        public void write(int c) throws IOException {
+            if (index >= nChars) flush();
+            buf[index++] = (char) c;
+        }
+
+        @Override
+        public void flush() throws IOException {
+            super.write(buf, 0, index);
+            index = 0;
+            super.flush(); // TODO 여기에 synchronized가 달려있어서 없애야한다
+        }
+
+        @Override
+        public void close() throws IOException {
+            flush();
+            super.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+        Writer w = new Writer();
+
+        int T = scan.nextInt();
+        while(T-- > 0) {
+            int N = scan.nextInt();
+//            int[] dp = new int[N + 1];
+            int max = Integer.MIN_VALUE;
+            int last = 0;
+            for(int i = 1; i <= N; i++) {
+                int n = scan.nextInt();
+                int current = Math.max(last + n, n);
+                max = Math.max(max, current);
+                last = current;
+            }
+            w.writeInt(max);
+            w.write('\n');
+        }
+        w.close();
+        // 1 2 -3 -4 -5 3 -1 -2 2
+    }
+}
+
