265067a904e0a4251a03e1727ac543da2b212217
GoldenMine0502 <ehe123@naver.com>
2024-07-13 04:10:39 +0900

    18353 DP

---
+++ src/kr/goldenmine/baekjoon/silver/silver2/p18353/Main.java
@@ -0,0 +1,126 @@
+package kr.goldenmine.baekjoon.silver.silver2.p18353;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char)c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32);
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -12345; }
+            if (c == 45) { c = read(); isMinus = true; }
+            else if (c == 46) { c = read(); }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c){
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static int lowerBound(List<Integer> arr, int key) {
+        int lo = 0;
+        int hi = arr.size();
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            /*
+             *  key 값이 중간 위치의 값보다 작거나 같을 경우
+             *
+             *  (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
+             */
+            if (key >= arr.get(mid)) {
+                hi = mid;
+            }
+
+            else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        int N = scan.nextInt();
+        List<Integer> LIS = new ArrayList<>();
+        LIS.add(999999999);
+
+        int size = 0;
+        for(int i=0;i<N;i++){
+            int num = scan.nextInt();
+            if(LIS.get(LIS.size() - 1) > num){
+                LIS.add(num);
+                size++;
+            } else {
+                int index = lowerBound(LIS, num);
+                LIS.set(index, num);
+            }
+        }
+        System.out.println(N - size);
+    }
+}
