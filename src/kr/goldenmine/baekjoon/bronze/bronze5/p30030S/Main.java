a3a05a538cde88faf8b61e1094b10a692a140af8
GoldenMine0502 <ehe123@naver.com>
2024-07-15 10:16:00 +0900

    30030번 구현

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze5/p30030S/Main.java
@@ -0,0 +1,11 @@
+package kr.goldenmine.baekjoon.bronze.bronze5.p30030S;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = new Scanner(System.in).nextInt();
+
+        System.out.println(N / 11 * 10);
+    }
+}
