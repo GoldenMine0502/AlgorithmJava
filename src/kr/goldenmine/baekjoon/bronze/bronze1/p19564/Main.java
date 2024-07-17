e4be83d1743898d10a5c59113ec454e509008b5d
GoldenMine0502 <ehe123@naver.com>
2024-07-18 08:16:26 +0900

    19564번 그리디

--- src/kr/goldenmine/baekjoon/bronze/bronze1/p19564/Main.java
+++ src/kr/goldenmine/baekjoon/bronze/bronze1/p19564/Main.java
@@ -1,3 +1,3 @@
 package kr.goldenmine.baekjoon.bronze.bronze1.p19564;
 
-interface Main{static void main(String[]args)throws Exception{byte[]b=new byte[100001];int N=System.in.read(b)-1,n=0,i=0;for(;i<N;++i){while(b[i]!=n%26+97)++n;++n;}System.out.println((n+25)/26);}}
+interface Main{static void main(String[]args)throws Exception{byte[]b=new byte[100001];int N=System.in.read(b)-2,n=1,i=0;for(;i<N;++i){if(b[i]>=b[i+1])++n;}System.out.print(n);}}
