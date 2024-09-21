3311007e9f7b9912c6f548cbc3c00ba364e4b48f
GoldenMine0502 <ehe123@naver.com>
2024-09-21 17:04:40 +0900

    1271번 구현

---
+++ src/kr/goldenmine/baekjoon/bronze/bronze5/p1271/Main.java
@@ -0,0 +1,15 @@
+package kr.goldenmine.baekjoon.bronze.bronze5.p1271;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
