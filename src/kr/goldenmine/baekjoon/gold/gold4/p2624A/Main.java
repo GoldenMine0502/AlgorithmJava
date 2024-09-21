d9cba0aa9997a1a204193168c992483c4fa94793
GoldenMine0502 <ehe123@naver.com>
2024-09-21 17:05:55 +0900

    2624번 DP

---
+++ src/kr/goldenmine/baekjoon/gold/gold4/p2624A/Main.java
@@ -0,0 +1,135 @@
+package kr.goldenmine.baekjoon.gold.gold4.p2624A;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c) {
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    static class Coin {
+        int price;
+        int count;
+
+        Coin(int price, int count) {
+            this.price = price;
+            this.count = count;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+
+        Coin[] coins = new Coin[K];
+
+        for (int i = 0; i < K; i++) {
+            coins[i] = new Coin(scan.nextInt(), scan.nextInt());
+        }
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+
+        for(int i = 0; i < K; i++) {
+            Coin c = coins[i];
+            for(int money = N; money > 0; money--) {
+
+                for(int cnt = 1; cnt <= c.count; cnt++) {
+                    int next = money - cnt * c.price;
+                    if(next >= 0)
+                        dp[money] += dp[next];
+                }
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
