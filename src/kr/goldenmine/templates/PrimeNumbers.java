866b4912961e0d06c2f1364c5c786cea6a3b2b05
GoldenMine0502 <ehe123@naver.com>
2024-10-05 01:57:57 +0900

    gcd / lcm 템플릿 추가

--- src/kr/goldenmine/templates/PrimeNumbers.java
+++ src/kr/goldenmine/templates/PrimeNumbers.java
@@ -100,4 +100,17 @@ public class PrimeNumbers {
     /*
     END OF MILLAR_RABIN PRIMALITY TEST
      */
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
 }
