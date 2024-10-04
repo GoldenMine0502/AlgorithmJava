f85973873f3ec282ac244a647693ea3b28bbec0a
GoldenMine0502 <ehe123@naver.com>
2024-10-05 02:03:44 +0900

    2745번 살짝 최적화

--- src/kr/goldenmine/baekjoon/bronze/bronze2/p2745S/Main.java
+++ src/kr/goldenmine/baekjoon/bronze/bronze2/p2745S/Main.java
@@ -2,7 +2,7 @@ package kr.goldenmine.baekjoon.bronze.bronze2.p2745S;
 
 public class Main {
     static class Reader {
-        final int SIZE = 1 << 13;
+        final int SIZE = 1 << 7;
         byte[] buffer = new byte[SIZE];
         int index, size;
 
@@ -15,53 +15,17 @@ public class Main {
             return sb.toString();
         }
 
-        char nextChar() throws Exception {
-            byte c;
-            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
-            return (char)c;
-        }
-
         int nextInt() throws Exception {
             int n = 0;
             byte c;
-            boolean isMinus = false;
             while ((c = read()) <= 32) { if (size < 0) return -1; }
-            if (c == 45) { c = read(); isMinus = true; }
             do n = (n << 3) + (n << 1) + (c & 15);
             while (isNumber(c = read()));
-            return isMinus ? ~n + 1 : n;
-        }
-
-        long nextLong() throws Exception {
-            long n = 0;
-            byte c;
-            boolean isMinus = false;
-            while ((c = read()) <= 32);
-            if (c == 45) { c = read(); isMinus = true; }
-            do n = (n << 3) + (n << 1) + (c & 15);
-            while (isNumber(c = read()));
-            return isMinus ? ~n + 1 : n;
-        }
-
-        double nextDouble() throws Exception {
-            double n = 0, div = 1;
-            byte c;
-            boolean isMinus = false;
-            while ((c = read()) <= 32) { if (size < 0) return -12345; }
-            if (c == 45) { c = read(); isMinus = true; }
-            else if (c == 46) { c = read(); }
-            do n = (n * 10) + (c & 15);
-            while (isNumber(c = read()));
-            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
-            return isMinus ? -n : n;
+            return n;
         }
 
         boolean isNumber(byte c) {
             return 47 < c && c < 58;
-        }
-
-        boolean isAlphabet(byte c){
-            return (64 < c && c < 91) || (96 < c && c < 123);
         }
 
         byte read() throws Exception {
@@ -91,6 +55,6 @@ public class Main {
                 num *= B;
 //            System.out.println(num);
         }
-        System.out.println(num);
+        System.out.print(num);
     }
 }
