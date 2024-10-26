efb76d92dd5d8c38686a782db7aa0215d5a0f072
GoldenMine0502 <ehe123@naver.com>
2024-10-26 21:00:53 +0900

    20413번 S2 그리디

---
+++ src/kr/goldenmine/baekjoon/silver/silver2/p20413/Main.java
@@ -0,0 +1,121 @@
+package kr.goldenmine.baekjoon.silver.silver2.p20413;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char)c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32);
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -12345; }
+            if (c == 45) { c = read(); isMinus = true; }
+            else if (c == 46) { c = read(); }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c){
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+        int N = scan.nextInt();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('B', scan.nextInt());
+        map.put('S', scan.nextInt());
+        map.put('G', scan.nextInt());
+        map.put('P', scan.nextInt());
+        map.put('D', map.get('P'));
+
+        String text = scan.nextString();
+
+        // BS = 29, 30
+        // BSG = 29, 30, 59
+
+        int[] arr = new int[N + 1];
+//        arr[1] = S - 1;
+
+        for(int i = 1; i <= N; i++) {
+            char grade = text.charAt(i - 1);
+
+            if(grade != 'D') {
+                if (i == 1) {
+                    int toPay = map.get(grade);
+                    arr[1] = toPay - 1;
+                } else {
+                    int toPay = map.get(grade);
+                    toPay -= arr[i - 1];
+                    arr[i] = toPay - 1;
+                }
+            } else {
+                arr[i] = map.get(grade);
+            }
+        }
+        int sum = 0;
+        for(int i = 1; i <= N; i++) {
+            sum += arr[i];
+        }
+//        System.out.println(Arrays.toString(arr));
+        System.out.println(sum);
+    }
+}
