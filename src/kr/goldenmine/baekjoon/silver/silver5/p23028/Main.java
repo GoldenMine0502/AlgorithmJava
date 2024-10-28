dcb9b1ada8a2a40643ae9e2eb9bf698889189f52
GoldenMine0502 <ehe123@naver.com>
2024-10-28 18:34:34 +0900

    23028 S5 구현

---
+++ src/kr/goldenmine/baekjoon/silver/silver5/p23028/Main.java
@@ -0,0 +1,110 @@
+package kr.goldenmine.baekjoon.silver.silver5.p23028;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char)c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32);
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -12345; }
+            if (c == 45) { c = read(); isMinus = true; }
+            else if (c == 46) { c = read(); }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c){
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        int N = scan.nextInt();
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        int[] 전공 = new int[10];
+        int[] 비전공 = new int[10];
+
+        for(int i = 0; i < 10; i++) {
+            전공[i] = scan.nextInt();
+            비전공[i] = scan.nextInt();
+        }
+
+        for(int i = N; i < 8; i++) {
+            int index = i - N;
+
+            int 전공고름 = Math.min(전공[index], 6);
+            int 비전공고름 = Math.min(비전공[index], 6 - 전공고름);
+
+            A += 전공고름 * 3;
+
+            B += 전공고름 * 3;
+            B += 비전공고름 * 3;
+//            System.out.println(전공고름 + ", " + 비전공고름 + ", " + A + ", " + B);
+        }
+
+        if(A >= 66 && B >= 130) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Nae ga wae");
+        }
+    }
+}
