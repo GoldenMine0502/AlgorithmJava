4d2b7fe774f19785769a91460c4147713d03d697
GoldenMine0502 <ehe123@naver.com>
2024-11-01 20:09:59 +0900

    2056 G4 위상 정렬

---
+++ src/kr/goldenmine/baekjoon/gold/gold4/p2056/Main.java
@@ -0,0 +1,132 @@
+package kr.goldenmine.baekjoon.gold.gold4.p2056;
+
+import java.util.*;
+
+public class Main {
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            do sb.appendCodePoint(c);
+            while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char)c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -1; }
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32);
+            if (c == 45) { c = read(); isMinus = true; }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) { if (size < 0) return -12345; }
+            if (c == 45) { c = read(); isMinus = true; }
+            else if (c == 46) { c = read(); }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c){
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader scan = new Reader();
+
+        int N = scan.nextInt();
+
+        int[] indegree = new int[N + 1];
+        int[] times = new int[N + 1];
+        int[] maxTimes = new int[N + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        List<List<Integer>> nodes = new ArrayList<>();
+
+        for(int i = 0; i <= N; i++) {
+            nodes.add(new ArrayList<>());
+        }
+
+        for(int i = 1; i <= N; i++) {
+            int time = scan.nextInt();
+            times[i] = time;
+
+            int count = scan.nextInt();
+            for(int j = 1; j <= count; j++) {
+                int out = scan.nextInt();
+                indegree[out]++;
+                nodes.get(i).add(out);
+            }
+        }
+
+        // 0인 노드로 시작
+        for(int i = 1; i <= N; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for(int next : nodes.get(current)) {
+                maxTimes[next] = Math.max(maxTimes[next], times[current]);
+                indegree[next]--;
+                if(indegree[next] == 0) {
+                    queue.add(next);
+                    times[next] += maxTimes[next];
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i = 1; i <= N; i++) {
+            max = Math.max(max, times[i]);
+        }
+//        System.out.println(Arrays.toString(times));
+        System.out.println(max);
+    }
+}
