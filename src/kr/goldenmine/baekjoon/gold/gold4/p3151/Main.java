8645d8de7312ab10868ac626d2101b18e1a07647
GoldenMine0502 <ehe123@naver.com>
2024-11-02 23:15:06 +0900

    3151번 G4 이분 탐색

--- src/kr/goldenmine/baekjoon/gold/gold4/p3151/Main.java
+++ src/kr/goldenmine/baekjoon/gold/gold4/p3151/Main.java
@@ -11,7 +11,9 @@ public class Main {
         String nextString() throws Exception {
             StringBuilder sb = new StringBuilder();
             byte c;
-            while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
             do sb.appendCodePoint(c);
             while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
             return sb.toString();
@@ -19,16 +21,21 @@ public class Main {
 
         char nextChar() throws Exception {
             byte c;
-            while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
-            return (char)c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
         }
 
         int nextInt() throws Exception {
             int n = 0;
             byte c;
             boolean isMinus = false;
-            while ((c = read()) <= 32) { if (size < 0) return -1; }
-            if (c == 45) { c = read(); isMinus = true; }
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
             do n = (n << 3) + (n << 1) + (c & 15);
             while (isNumber(c = read()));
             return isMinus ? ~n + 1 : n;
@@ -38,8 +45,11 @@ public class Main {
             long n = 0;
             byte c;
             boolean isMinus = false;
-            while ((c = read()) <= 32);
-            if (c == 45) { c = read(); isMinus = true; }
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
             do n = (n << 3) + (n << 1) + (c & 15);
             while (isNumber(c = read()));
             return isMinus ? ~n + 1 : n;
@@ -49,12 +59,22 @@ public class Main {
             double n = 0, div = 1;
             byte c;
             boolean isMinus = false;
-            while ((c = read()) <= 32) { if (size < 0) return -12345; }
-            if (c == 45) { c = read(); isMinus = true; }
-            else if (c == 46) { c = read(); }
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
             do n = (n * 10) + (c & 15);
             while (isNumber(c = read()));
-            if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
             return isMinus ? -n : n;
         }
 
@@ -62,7 +82,7 @@ public class Main {
             return 47 < c && c < 58;
         }
 
-        boolean isAlphabet(byte c){
+        boolean isAlphabet(byte c) {
             return (64 < c && c < 91) || (96 < c && c < 123);
         }
 
@@ -75,21 +95,109 @@ public class Main {
         }
     }
 
+    // 내림차순 기준 1 2 3 4 5
+    // 완벽하게 같은 값 찾을 때
+    public static int binarySearch(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (true) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == value) {
+                return mid;
+            } else if (arr[mid] > value) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+            if (left > right) return -1;
+        }
+    }
+
+    public static int lowerBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            /*
+             *  key 값이 중간 위치의 값보다 작거나 같을 경우
+             *
+             *  (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
+             */
+            if (key <= arr[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return lo;
+    }
+
+    public static int upperBound(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length;
+
+        // lo가 hi랑 같아질 때 까지 반복
+        while (lo < hi) {
+            int mid = (lo + hi) / 2; // 중간위치를 구한다.
+
+            // key값이 중간 위치의 값보다 작을 경우
+            if (key < arr[mid]) {
+                hi = mid;
+            }
+            // 중복원소의 경우 else에서 처리된다.
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
     public static void main(String[] args) throws Exception {
         Reader scan = new Reader();
 
         int N = scan.nextInt();
 
         int[] arr = new int[N];
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = scan.nextInt();
         }
 
         Arrays.sort(arr);
+//        System.out.println(Arrays.toString(arr));
 
-        int left = 0;
-        int right = arr.length - 1;
+        long count = 0;
 
-        // 여기서 이분탐색 결과랑 종합하면 되겠지만 귀찮아 죽겠네
+        for (int A = 0; A < N; A++) {
+            for (int B = A + 1; B < N; B++) {
+                int sum = arr[A] + arr[B];
+
+                // C를 결정하기
+                int lower = Math.max(B + 1, lowerBound(arr, -sum));
+                int upper = upperBound(arr, -sum);
+
+//                System.out.println(arr[A] + ", " + arr[B] + ", " + lower + ", " + upper);
+
+                if(upper > lower)
+                    count += upper - lower;
+
+//                for (int C = lower; C < upper; C++) {
+//                    if (arr[A] + arr[B] + arr[C] == 0) {
+//                        count++;
+////                            System.out.println(A + ", " + B + ", " + C + ", " + arr[A] + ", " + arr[B] + ", " + arr[C]);
+//                    }
+//                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
